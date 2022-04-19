8361deaac3ddb95973a2a032a0e0f4202303c5ff
Aman kumar <87026675+Aman300@users.noreply.github.com>
2022-04-19 20:43:36 +0530

    assignment/done

--- src/controllers/batchController.js
+++ src/controllers/batchController.js
@@ -26,13 +26,19 @@ const scholarship_developers= async function (req, res) {
 //this part is not complet
 
 const developers= async function (req, res) {
-     let batch = req.query.percentage
-     let developer = req.query.program
-     let value = await developerModel.find({percentage: {$gte: batch}}).select({_id: 1})
-     let Storevalue  = await batchModel.find({program:{$eq: developer}})
+     let percentage = req.query.percentage
+     let program = req.query.program
+     let value = await batchModel.find({name:program}).select({_id: 1})
+
+     let store = []
+     for(let i = 0; i < value.length; i++){
+         let objid = value[i]._id
+         store.push(objid)
+     }
+     let fetchData  = await developerModel.find({$and: [ {batch:{$in: store}},{percentage:{$gt:percentage}} ]}).populate('batch')
 
      //let finalData = await batchModel.find(value)
-    res.send({data: value})
+    res.send({data: fetchData})
 
 }
      
