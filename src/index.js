4292cee32827af9018aa682f24dd7da95591a71e
Aman kumar <87026675+Aman300@users.noreply.github.com>
2022-04-23 19:52:05 +0530

    assignment/auth/note/done

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Aman300:ByXZ2qfTNQNWF7Uj@cluster0.o4rcy.mongodb.net/Aman_Auth_check-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -17,6 +17,6 @@ mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.m
 app.use('/', route)
 
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(process.env.PORT || 2001, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 2001))
 });
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,22 @@
+const jwt = require("jsonwebtoken");
+const userController = require("../controllers/userController")
 const authenticate = function(req, req, next) {
     //check the token in request header
     //validate this token
+   
+
+    let token = req.headers["x-Auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+
+    //If no token is present in the request header return error
+    if (!token) return res.send({ status: false, msg: "token must be present" });
+
+    let decodedToken = jwt.verify(token, "functionup-thorium");
+    if (!decodedToken)
+    return res.send({ status: false, msg: "token is invalid" });
+
+    return decodedToken;
+
 
     next()
 }
@@ -8,5 +24,14 @@ const authenticate = function(req, req, next) {
 
 const authorise = function(req, res, next) {
     // comapre the logged in user's id and the id in request
+    let userId = req.params.userId
+    //userId for the logged-in user
+    let decodeUserId =  authenticate.decodedToken
+    if(userId != decodeUserId) return res.send({ status: false, msg: "error"})
+    //return decodeUserId;
+
     next()
 }
+
+module.exports.authenticate = authenticate;
+module.exports.authorise = authorise;
